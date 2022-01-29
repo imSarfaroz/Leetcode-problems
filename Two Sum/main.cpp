5e9c7a3f96de523df95cbf01d7a32807975df812
imsarfaroz <sarfarozyunusov@gmail.com>
2022-01-29 15:32:06 +0600

    main

---
+++ Two Sum/main.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+class Solution {
+public:
+    int a=0,b=0;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        for(int i=0;i<nums.size()-1;i++){
+            
+            for(int j=i+1;j<nums.size();j++){
+                
+                if(nums[i]+nums[j] == target){
+                    
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        return {a,b};
+    }
+};
