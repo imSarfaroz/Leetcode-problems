f000b533198883365912c60a4d4b11460dcd255a
imsarfaroz <sarfarozyunusov@gmail.com>
2022-02-01 18:08:22 +0600

    swift solution accepted

---
+++ Running Sum of 1d Array/main.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func runningSum(_nums: [Int]) -> [Int]{
+        var n:[Int] = [Int]()
+        var t = 0
+        for i in nums{
+            t += i
+            n.append(t)
+        }
+        return n
+    }
+}
