5cf7451698e3e826a74e82189748fffa8826394c
imsarfaroz <sarfarozyunusov@gmail.com>
2022-02-05 12:50:26 +0600

    swift solution

---
+++ Permutaitons with array/main.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class Solution {
+    func buildArray(_ nums: [Int]) -> [Int] {
+        var ans = [Int]()
+        for i in 0..<nums.count{
+            ans.append(nums[nums[i]])
+        }
+        return ans
+    }
+}
