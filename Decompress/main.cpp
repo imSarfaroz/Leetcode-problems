23e4606db8c3b24a19e6d29ea31a1d99f28396bc
imsarfaroz <sarfarozyunusov@gmail.com>
2022-02-14 23:30:07 +0600

    C++ solution

---
+++ Decompress/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> arr;
+        
+        for(int i = 0; i < nums.size(); i+=2){
+            for(int j = 0; j < nums[i]; j++){
+                arr.push_back(nums[i+1]);
+            }
+        }
+        return arr;
+    }
+};
+
+
+// class Solution {
+// public:
+//     vector<int> decompressRLElist(vector<int>& nums) {
+//         vector<int> arr;
+        
+//         for(int i = 0; i < nums.size(); i+=2){
+//             arr.insert(arr.end(), nums[i], nums[i+1]);
+//         }
+        
+//         return arr;
+//     }
+// };
