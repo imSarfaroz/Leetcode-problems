2eeb646e631051590bc44a0b619aa04a2c2d50d2
imsarfaroz <sarfarozyunusov@gmail.com>
2022-02-18 18:09:17 +0600

    c++ solution

---
+++ Create Target Array in the Given Order/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> arr;
+        
+        for(int i = 0; i <  nums.size(); i++){
+            arr.insert(arr.begin() + index[i], nums[i]);
+        }
+        return arr;
+    }
+};
