2e407c9931ee51e030120f5a2e2303f7801449be
imsarfaroz <sarfarozyunusov@gmail.com>
2022-02-23 14:36:57 +0600

    c++ solution

---
+++ Roman to Integer/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> table ={
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+        };
+        
+        int sum = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(table[s[i]] < table[s[i+1]]){
+                sum-=table[s[i]];
+            }
+            else{
+                sum+=table[s[i]];
+            }
+        }
+        return sum;
+    }
+};
