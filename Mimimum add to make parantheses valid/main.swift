8ac6948bbcbbfdf0f6092a28d75b24b2461f4371
imsarfaroz <sarfarozyunusov@gmail.com>
2022-03-21 12:35:53 +0600

    swift sol

---
+++ Mimimum add to make parantheses valid/main.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func minAddToMakeValid(_ s: String) -> Int {
+        var stack = [Character]()
+        var count:Int = 0
+        
+        for char in s {
+            if(char == "(") {
+                stack.append(char);
+            } else {
+                if(stack.count > 0) {
+                    stack.removeLast()
+                } else {
+                    count+=1
+                }
+            }
+        }
+        return count + stack.count
+    }
+}
