edaa92190383cb046b578d18dcf4b99892088a95
Sarfaroz Yunusov <58271101+imSarfaroz@users.noreply.github.com>
2022-05-17 10:39:04 -0400

    Create main.swift

---
+++ RemoveDuplicates/main.swift
@@ -0,0 +1,18 @@
+class Solution {
+func removeDuplicates(_ nums: inout [Int]) -> Int { 
+        if nums.count == 0
+        {
+            return 0
+        }
+        var index  = 1
+        for i in 1..<nums.count
+        {
+            if nums[i - 1] != nums[i]
+            {
+                nums[index] = nums[i]
+                index += 1
+            }
+        }
+        return index
+    }
+}
