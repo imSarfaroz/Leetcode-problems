0f6f1b9fda5cb96c91fa0a72db7ab36fd7298265
Sarfaroz Yunusov <58271101+imSarfaroz@users.noreply.github.com>
2022-05-18 06:41:58 -0400

    Create main.cpp

---
+++ MergeTwoSortedLists/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+       
+        ListNode h(INT_MIN);
+        
+        ListNode *tail = &h;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+                
+            } else {
+                
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return h.next;
+    }
+};
